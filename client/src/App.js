c585be78a5c9e1b65f650e1d7c9bd4ec21aabf21
Cal <calvinctvodesign@gmail.com>
2019-08-18 14:30:02 -0700

    merge Michael's branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Home from "./component/Home";
 import "./App.css";
 import Carousel from './component/Carousel'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          <Carousel/>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Home />
     );
   }
 }
