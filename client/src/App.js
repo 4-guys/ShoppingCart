3d58f7c048d6b5be7c8329ef4c9b2c0375a15e55
Calvin Vo <calvinctvodesign@gmail.com>
2019-08-19 11:18:35 -0700

    change the routes for home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,19 @@
 import React, { Component } from "react";
-import Home from "./component/Home";
 import "./App.css";
-import Carousel from './component/Carousel'
 import NavBar from './component/NavBar'
-import ProductCard from './component/ProductCard'
-import Container from "@material-ui/core/Container"
-import Grid from '@material-ui/core/Grid';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './pages/Home'
+
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-
-      <NavBar />
-      <Container maxWidth="md">
-
-        <Carousel />
-      </Container>
-      <Grid
-        container
-        direction="row"
-        justify="center"
-        alignItems="center"
-        spacing={2}
-      >
-
-        <Grid item ><ProductCard /></Grid>
-        <Grid item ><ProductCard /></Grid>
-        <Grid item ><ProductCard /></Grid>
-      </Grid>
-
-
-</React.Fragment>
+      <Router>
+        <div>
+          <NavBar />
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/home" component={Home}/>
+        </div>
+      </Router>
 
     )
   }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import Carousel from '../component/Carousel'
+import ProductCard from '../component/ProductCard'
+import Container from "@material-ui/core/Container"
+import Grid from '@material-ui/core/Grid';
+
+function Home() {
+    return (
+        <React.Fragment>
+
+            <Container maxWidth="md">
+
+                <Carousel />
+            </Container>
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                spacing={2}
+            >
+
+                <Grid item ><ProductCard /></Grid>
+                <Grid item ><ProductCard /></Grid>
+                <Grid item ><ProductCard /></Grid>
+            </Grid>
+
+
+        </React.Fragment>
+    );
+}
+
+export default Home;
