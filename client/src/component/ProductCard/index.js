9f7b9ba94fbc989772022157d63b7e4f9714634a
Michael Eguina <michael@corner-exit.com>
2019-08-20 10:44:12 -0700

    link button added to product card

--- client/src/component/ProductCard/index.js
+++ client/src/component/ProductCard/index.js
@@ -8,50 +8,51 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles(theme =>({
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-},
-card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-},
-cardMedia: {
-    paddingTop: '56.25%', // 16:9
-},
-cardContent: {
-    flexGrow: 1,
-},
+const useStyles = makeStyles(theme => ({
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    cardMedia: {
+        paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
 }))
 
 export default function MediaCard() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <Card className={classes.card}>
-    <CardMedia
-        className={classes.cardMedia}
-        image="https://source.unsplash.com/random"
-        title="Image title"
-    />
-    <CardContent className={classes.cardContent}>
-        <Typography gutterBottom variant="h5" component="h2">
-            Item name (will input with props)
+    return (
+        <Card className={classes.card}>
+            <CardMedia
+                className={classes.cardMedia}
+                image="https://source.unsplash.com/random"
+                title="Image title"
+            />
+            <CardContent className={classes.cardContent}>
+                <Button href="/productDetails">
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Item name (will input with props)
+                    </Typography>
+                </Button>        <Typography>
+                    This is a media card. You can use this section to describe the content.
 </Typography>
-        <Typography>
-            This is a media card. You can use this section to describe the content.
-</Typography>
-    </CardContent>
-    <CardActions>
-        <Button size="small" color="primary">
-            Add to Cart
+            </CardContent>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Add to Cart
 </Button>
-        <Button href="/checkout" size="small" color="primary">
-            Checkout
+                <Button href="/checkout" size="small" color="primary">
+                    Checkout
 </Button>
-    </CardActions>
-</Card>
-  );
+            </CardActions>
+        </Card>
+    );
 }
