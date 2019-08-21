7d7894a06de888b72f409e5af0e3d6fe72ea11c3
Michael Eguina <michael@corner-exit.com>
2019-08-21 15:37:36 -0700

    merged with tai2

--- server.js
+++ server.js
@@ -41,10 +41,11 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
+app.use(routes);
+
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
-app.use(routes);
 
 
 // app.listen(PORT, function() {
