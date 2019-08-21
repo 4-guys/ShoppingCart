db34967eff5c59e5bf1886ee55ee6b537226fefb
Tai <taing89@gmail.com>
2019-08-21 15:20:11 -0700

    Login now works. Change login to logout

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,6 +10,10 @@ export default {
         return axios.get("/api/items/" + itemName).then(results => results.data);
     },
 
+    loginUser: function (newUser) {
+        return axios.post("/api/login/", newUser).then(results => results.data);
+    },
+
     signUpUser: function (newUser) {
         return axios.post("/api/signup/", newUser).then(results => results.data);
     }
