f498678d9fe7ffd4565e4a019b7e9eed11a46b67
Michael Eguina <michael@corner-exit.com>
2019-08-26 11:41:49 -0700

    cart prints out on review order

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,38 @@
+import API from '../utils/API'
+
 export const addToCart = (productId) =>  {
+//   async function Shopping(){
+//     await API.getItems().then(items=> itemsAll=items)
+//         let shoppingCart = localStorage.getItem('cart');
+//       var arr= shoppingCart.split(",")
+//       var purchasedProdId=[]
+//       for(var i=1;i<arr.length-1;i++){
+//             var keyVal=arr[i].split(':')
+//             var str2 = keyVal[0].replace (/"/g, "")
+//           purchasedProdId.push(str2)
+//       }
+//       for(var j=0;j<itemsAll.length;j++){
+//           for(var k=0;k<purchasedProdId.length;k++){
+//           if(j==purchasedProdId[k])  
+//           {
+//               var newProduct= { name: itemsAll[j].brandName, desc: itemsAll[j].itemDescription, price: itemsAll[j].ourPrice }
+//               products.push(newProduct)
+//               total= total + itemsAll[j].ourPrice
+//               console.log(newProduct)
+//           }
+//         }
+//       }
+//       console.log(products)
+//       console.log(total)
+// };
+let itemsAll;
+let total;
+let products=[]
+// Shopping()
+
+
+
+
     let cart = localStorage.getItem('cart') 
                   ? JSON.parse(localStorage.getItem('cart')) : {};
     let id = productId.toString();
--- client/src/pages/Review.js
+++ client/src/pages/Review.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
@@ -9,38 +9,33 @@ import API from '../utils/API';
 import { pseudoRandomBytes } from 'crypto';
 import { Card } from '@material-ui/core';
 
-//// local storage cart
-// getItems = () => {    API.getItems().then(function(res){
-//         itemsAll=res
-//     })
-// }
+// local storage cart
 
-async function cart(){
-    await API.getItems().then(items=> itemsAll=items)
-        let cart = localStorage.getItem('cart');
-      console.log(itemsAll)
-      var arr= cart.split(",")
-      var purchasedProdId=[]
-      for(var i=1;i<arr.length-1;i++){
-            var keyVal=arr[i].split(':')
-            var str2 = keyVal[0].replace (/"/g, "")
-          console.log(str2 + "quantity: "+keyVal[1])
-          purchasedProdId.push(str2)
-      }
-      for(var j=0;j<itemsAll.length;j++){
-          for(var k=0;k<purchasedProdId.length;k++){
-          if(j==purchasedProdId[k])  
-          {
-              console.log(itemsAll[j].brandName)
-              var newProduct= { name: itemsAll[j].brandName, desc: itemsAll[j].itemDescription, price: itemsAll[j].ourPrice }
-              products.push(newProduct)
-              console.log(newProduct)
-          }
-        }
-      }
-      productPush=true
-      console.log(products)
-};
+
+// async function cart(){
+//     await API.getItems().then(items=> itemsAll=items)
+//         let cart = localStorage.getItem('cart');
+//       var arr= cart.split(",")
+//       var purchasedProdId=[]
+//       for(var i=1;i<arr.length-1;i++){
+//             var keyVal=arr[i].split(':')
+//             var str2 = keyVal[0].replace (/"/g, "")
+//           purchasedProdId.push(str2)
+//       }
+//       for(var j=0;j<itemsAll.length;j++){
+//           for(var k=0;k<purchasedProdId.length;k++){
+//           if(j==purchasedProdId[k])  
+//           {
+//               var newProduct= { name: itemsAll[j].brandName, desc: itemsAll[j].itemDescription, price: itemsAll[j].ourPrice }
+//               products.push(newProduct)
+//               total= total + itemsAll[j].ourPrice
+//           }
+//         }
+//       }
+//       productPush=true
+//       console.log(total)
+//       console.log(products)
+// };
 
 
  
@@ -49,11 +44,12 @@ async function cart(){
 let itemsAll;
 let productPush= false
 const products = [
-    { name: 'Product 2', desc: 'Another thing', price: '$3.45' },
-    { name: 'Product 3', desc: 'Something else', price: '$6.51' },
-    { name: 'Product 4', desc: 'Best thing of all', price: '$14.11' },
-    { name: 'Shipping', desc: '', price: 'Free' },
+    // { name: 'Product 2', desc: 'Another thing', price: '$3.45' },
+    // { name: 'Product 3', desc: 'Something else', price: '$6.51' },
+    // { name: 'Product 4', desc: 'Best thing of all', price: '$14.11' },
+    // { name: 'Shipping', desc: '', price: 'Free' },
 ];
+let total= 0
 const addresses = ['1 Material-UI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
 const payments = [
     { name: 'Card type', detail: 'Visa' },
@@ -73,17 +69,62 @@ const useStyles = makeStyles(theme => ({
         marginTop: theme.spacing(2),
     },
 }));
-cart()
-export default  function Review() {
-    const classes = useStyles();
 
+export default function Review() {
+    const classes = useStyles();
+    let shoppingCart = products
+    const [state, setState] = useState(shoppingCart);
+    
+async function cart(){
+    await API.getItems().then(items=> itemsAll=items)
+        let cart = localStorage.getItem('cart');
+      var arr= cart.split(",")
+      var purchasedProdId=[]
+      for(var i=1;i<arr.length-1;i++){
+            var keyVal=arr[i].split(':')
+            var str2 = keyVal[0].replace (/"/g, "")
+          purchasedProdId.push(str2)
+      }
+      for(var j=0;j<itemsAll.length;j++){
+          for(var k=0;k<purchasedProdId.length;k++){
+          if(j==purchasedProdId[k])  
+          {
+              var newProduct= { name: itemsAll[j].brandName, desc: itemsAll[j].itemDescription, price: itemsAll[j].ourPrice }
+              products.push(newProduct)
+              total= total + itemsAll[j].ourPrice
+          }
+        }
+      }
+    
+      productPush=true
+      console.log(total)
+      console.log(products)
+
+      setState(shoppingCart => {
+    // cart()
+// Object.assign would also work
+return {...shoppingCart, ...shoppingCart};
+});
+
+};
+
+          useEffect(()=>{
+            // {setState=products}
+            cart()
+
+        shoppingCart={state}
+        // cart()
+
+    }, [])
+    
     return (
+    
         <React.Fragment>
             <Typography variant="h6" gutterBottom>
                 Order summary
       </Typography>
             <List disablePadding>
-                {products.map(product => (
+                {shoppingCart.map(product => (
                     <ListItem className={classes.listItem} key={product.name}>
                         <ListItemText primary={product.name} secondary={product.desc} />
                         <Typography variant="body2">{product.price}</Typography>
@@ -92,7 +133,7 @@ export default  function Review() {
                 <ListItem className={classes.listItem}>
                     <ListItemText primary="Total" />
                     <Typography variant="subtitle1" className={classes.total}>
-                        $34.06
+                        ${total}
           </Typography>
                 </ListItem>
             </List>
